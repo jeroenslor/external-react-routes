713ec28a8c0300d6769238c87c5ed91926025517
Jeroen Slor <jeroen@jeroenslor.com>
2017-08-31 22:24:07 +0200

    Started with homeview and connected with backend

--- website/src/HomeView.js
+++ website/src/HomeView.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-function HomeView({ content: { foo } }) {
-    return (
-      <div>
-        <h1>Home template</h1>
-        <div>some content: {foo}</div>
-        <Link to='/foo'>go to foo</Link>
-      </div>
-    )
-  }
+function HomeView({ content }) {
+  return (
+    <section className="section section--full-height background-image-full overlay overlay--dark section--content-center section--thick-border"
+      style={{backgroundImage: content.heroBackgroundImageUrl ? `url('${content.heroBackgroundImageUrl}')` : null}}>
+      <div className="section__hero-content">
+        <h1>{content.heroHeader}</h1>
+        <p className="section__description">{content.heroDescription}</p>
+        <Link className='class="button button--border--solid"' to={content.heroCtaUrl}>
+          {content.heroCtacaption}
+        </Link>
 
-  export default HomeView
+      </div>
+    </section>
+    )
+}
+
+export default HomeView
